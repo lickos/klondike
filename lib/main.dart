f1adff4428341cd5020608d5031aad4dfcbf07f5
lickos <george.chios@gmail.com>
2024-02-06 20:30:25 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  final game = FlameGame();
+  runApp(
+    GameWidget(
+      game: game,
+    ),
+  );
+}
