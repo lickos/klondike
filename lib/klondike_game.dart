7af1715189b0f26d68d3698cd1a9d9ba7c6fd998
lickos <g.sgoutas@chios.local>
2024-02-07 13:53:34 +0200

    cards

--- lib/klondike_game.dart
+++ lib/klondike_game.dart
@@ -1,24 +1,26 @@
-import 'dart:async';
+import 'dart:math';
 
 import 'package:flame/components.dart';
-import 'package:flame/extensions.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'package:klondike/components/foundation.dart';
-import 'package:klondike/components/pile.dart';
-import 'package:klondike/components/stock.dart';
-import 'package:klondike/components/waste.dart';
+
+import 'components/card.dart';
+import 'components/foundation.dart';
+import 'components/pile.dart';
+import 'components/stock.dart';
+import 'components/waste.dart';
 
 class KlondikeGame extends FlameGame {
+  static const double cardGap = 175.0;
   static const double cardWidth = 1000.0;
   static const double cardHeight = 1400.0;
-  static const double cardGap = 175.0;
   static const double cardRadius = 100.0;
   static final Vector2 cardSize = Vector2(cardWidth, cardHeight);
 
   @override
-  FutureOr<void> onLoad() async {
+  Future<void> onLoad() async {
     await Flame.images.load('klondike-sprites.png');
+
     final stock = Stock()
       ..size = cardSize
       ..position = Vector2(cardGap, cardGap);
@@ -37,8 +39,8 @@ class KlondikeGame extends FlameGame {
       (i) => Pile()
         ..size = cardSize
         ..position = Vector2(
-          cardGap + i * (cardGap + cardWidth),
-          2 * cardGap + cardHeight,
+          cardGap + i * (cardWidth + cardGap),
+          cardHeight + 2 * cardGap,
         ),
     );
 
@@ -48,16 +50,29 @@ class KlondikeGame extends FlameGame {
     world.addAll(piles);
 
     camera.viewfinder.visibleGameSize =
-        Vector2(cardHeight * 7 * cardGap * 8, 4 * cardHeight + 3 * cardGap);
+        Vector2(cardWidth * 7 + cardGap * 8, 4 * cardHeight + 3 * cardGap);
     camera.viewfinder.position = Vector2(cardWidth * 3.5 + cardGap * 4, 0);
     camera.viewfinder.anchor = Anchor.topCenter;
-  }
 
-  Sprite klondikeSprite(double x, double y, double width, double height) {
-    return Sprite(
-      Flame.images.fromCache('klondike-sprite.png'),
-      srcPosition: Vector2(x, y),
-      srcSize: Vector2(width, height),
-    );
+    final random = Random();
+    for (var i = 0; i < 7; i++) {
+      for (var j = 0; j < 4; j++) {
+        final card = Card(random.nextInt(13) + 1, random.nextInt(4))
+          ..position = Vector2(100 + i * 1150, 100 + j * 1500)
+          ..addToParent(world);
+        // flip the card face-up with 90% probability
+        if (random.nextDouble() < 0.9) {
+          card.flip();
+        }
+      }
+    }
   }
+}
+
+Sprite klondikeSprite(double x, double y, double width, double height) {
+  return Sprite(
+    Flame.images.fromCache('klondike-sprites.png'),
+    srcPosition: Vector2(x, y),
+    srcSize: Vector2(width, height),
+  );
 }
---
+++ lib/suit.dart
@@ -0,0 +1,28 @@
+import 'package:flame/components.dart';
+import 'package:flutter/foundation.dart';
+import 'klondike_game.dart';
+
+@immutable
+class Suit {
+  factory Suit.fromInt(int index) {
+    assert(index >= 0 && index <= 3);
+    return _singletons[index];
+  }
+
+  Suit._(this.value, this.label, double x, double y, double w, double h)
+      : sprite = klondikeSprite(x, y, w, h);
+
+  final int value;
+  final String label;
+  final Sprite sprite;
+
+  static final List<Suit> _singletons = [
+    Suit._(0, '♥', 1176, 17, 172, 183),
+    Suit._(1, '♦', 973, 14, 177, 182),
+    Suit._(2, '♣', 974, 226, 184, 172),
+    Suit._(3, '♠', 1178, 220, 176, 182),
+  ];
+
+  bool get isRed => value <= 1;
+  bool get isBlack => value >= 2;
+}
